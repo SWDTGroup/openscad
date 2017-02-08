d982717d5f39934bac778180d9b2ea467a22b826
zwbrush <zwbrush@hotmail.com>
2017-02-08 17:27:50 +0800

    change to fit master version

--- src/polarizationNode.h
+++ src/polarizationNode.h
@@ -2,12 +2,13 @@
 #pragma once
 
 #include "node.h"
-#include "visitor.h"
+#include "NodeVisitor.h"
 #include "linalg.h"
 
 class PolarizationNode : public AbstractNode
 {
 public:
+	VISITABLE();
 	PolarizationNode(const ModuleInstantiation *mi) : AbstractNode(mi) { }
   virtual Response accept(class State &state, Visitor &visitor) const {
 		return visitor.visit(state, *this);
